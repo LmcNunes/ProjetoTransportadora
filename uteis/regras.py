69d69a950840e00ca07a0bdf3cda92554ef6532b
LmcNunes <107082872+LmcNunes@users.noreply.github.com>
2022-10-26 01:41:00 -0300

    Update regras.py

--- uteis/regras.py
+++ uteis/regras.py
@@ -2,28 +2,28 @@ from uteis.globais import message
 from uteis.globais import messageEmpresas
 
 def verificarCondicao(altura,largura,peso):
-  if(altura >= 10 and altura <= 140 and peso >0):
-      if(largura >= 6 and largura <= 125):
-          valorFrete = (peso * 0.3) / 10
-          return  message("Entrega Kabum", valorFrete, dias=4)
+  if altura >= 5 and altura <= 140 and peso > 0:
+      if(largura >= 13 and largura <= 125):
+          valorFreteN = (peso * 0.3) / 10
+          valorFreteK = (peso * 0.2) / 10
+          return messageEmpresas("Entrega Ninja", valorFreteN, 6, "Entrega Kabum",valorFreteK,4) 
           pass
       pass
 
   elif altura >= 10 and altura <= 200 and peso >0:
       if(largura >= 6 and largura <= 140):
-          valorFrete = (peso * 0.2) / 10
+          valorFrete = (peso * 0.3) / 10
           return message("Entrega Ninja ", valorFrete, 6)
           pass
       pass
 
-
-  elif altura >= 5 and altura <= 140 and peso > 0:
+  elif(altura >= 5 and altura <= 140 and peso >0):
       if(largura >= 13 and largura <= 125):
-          valorFreteK = (peso * 0.5) / 10
-          valorFreteN = (peso * 0.2) / 10
-          return messageEmpresas("Entrega Kabum", valorFreteK, 4, "Entrega Ninja ",valorFreteN,6,) 
+          valorFrete = (peso * 0.2) / 10
+          return  message("Entrega Kabum", valorFrete, 4)
           pass
       pass
+
 # pensando em um cenário de usuário retornei uma mensagem para não parecer um problema de tela, mesmo sabendo que é uma função de front-end
   else:
         return "[Não temos suporte para este produto ainda]"
